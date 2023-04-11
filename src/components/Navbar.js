2c18abe5bbed5d8dd7a272ed32db97d65331302c
Praneethh2001 <94681968+Praneethh2001@users.noreply.github.com>
2023-04-11 19:55:57 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import {Link} from 'react-router-dom'
     return (
         <nav className="navbar fixed-top navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">NewsMonkey</Link>
+          <Link className="navbar-brand" to="/">News-Instant</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
